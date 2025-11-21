2b3cb51f83b57211d57835e40acb1da89d4b4f67
David Bobes Gutierrez <jsw03280@educastur.es>
2025-11-21 17:28:20 +0100

    Initial commit

---
+++ src/main/java/com/example/EamenMocDBG/EamenMocDbgApplication.java
@@ -0,0 +1,13 @@
+package com.example.EamenMocDBG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EamenMocDbgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EamenMocDbgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EamenMocDBG/EamenMocDbgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EamenMocDBG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EamenMocDbgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
