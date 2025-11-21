1dc5e7d56bc5322b8c20fb2edf30ad12f29e0045
David Bobes Gutierrez <jsw03280@educastur.es>
2025-11-21 18:41:55 +0100

    Añadidos los metodos necesarios para que el proyecto arranque

--- src/main/java/com/example/EamenMocDBG/service/ProductoServiceImpl.java
+++ src/main/java/com/example/EamenMocDBG/service/ProductoServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.EamenMocDBG.service;
 import com.example.EamenMocDBG.entity.Producto;
 import com.example.EamenMocDBG.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ProductoServiceImpl implements ProductoService {
 
     @Autowired
